339132a82c104258edaa0d71e65b0ae29b58d91b
Henrique Tavares <ihenrits@gmail.com>
2018-10-01 15:17:38 -0300

    Criação da API REST

---
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,6 @@
+const BillingCycle = required('./billingCycle')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({new: true, runValidators: true})
+
+module.exports = BillingCycle
