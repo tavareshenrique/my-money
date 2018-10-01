4d7fc86238be188b07db6ddb204efac7aa54469d
Henrique Tavares <ihenrits@gmail.com>
2018-10-01 16:15:24 -0300

    Outras melhorias e testes na API

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,4 +1,4 @@
-const BillingCycle = required('./billingCycle')
+const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
