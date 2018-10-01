d23ea593e38c5c08f69ba69a8d07c79048a3efe1
Henrique Tavares <ihenrits@gmail.com>
2018-10-01 17:28:09 -0300

    Middleware Query Parser Int | Correção no CORS

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,9 +4,12 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const qeueryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extend: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(qeueryParser())
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
