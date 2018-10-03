de57095afbab16dd8fe0ce2c803fc2cf7e29aa82
Henrique Tavares <ihenrits@gmail.com>
2018-10-03 10:54:06 -0300

    Componente Cabeçalho(Header))

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,10 @@
 import '../common/template/dependecies'
 import React from 'react'
 
+import Header from '../common/template/header'
+
 export default props => (
-    <div className='wrapper'>
-        <h1>App</h1>
+    <div className='wrapper' >
+        <Header />
     </div>
 )
