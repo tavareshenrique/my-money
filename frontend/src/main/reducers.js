af41716efee98a7adfdd9a839060a73f6f3d20ea
Henrique Tavares <ihenrits@gmail.com>
2018-10-08 21:54:49 -0300

    Componente TabHeader: Conectando com Redux

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
+import TabReducer from '../common/tab/tabReducer'
 
 const rootReducer = combineReducers({
 
     /* dashboard: () => ({summary: {credit: 100, debt: 50}}) */
 
-    dashboard: DashboardReducer
+    dashboard: DashboardReducer,
+    tab: TabReducer
 
 })
 
