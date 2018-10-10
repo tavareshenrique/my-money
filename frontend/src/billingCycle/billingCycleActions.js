cfd8854448053b5dab5f4787e6908eec7021cf83
Henrique Tavares <ihenrits@gmail.com>
2018-10-10 16:01:58 -0300

    Criando a Action getList e o Reducer

---
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export function getList() {
+    const request = axios.get(`${BASE_URL}/billingCycles`)
+    return {
+        type: 'BILLING_CYCLES_FETCHED',
+        payload: request
+    }
+}
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,13 +2,13 @@ import { combineReducers } from 'redux'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
 
-    /* dashboard: () => ({summary: {credit: 100, debt: 50}}) */
-
-    dashboard: DashboardReducer,
-    tab: TabReducer
+    dashboard: DashboardReducer, /* dashboard: () => ({summary: {credit: 100, debt: 50}}) */
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer
 
 })
 
