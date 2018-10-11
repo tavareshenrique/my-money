d8bc98a6c8e2adf54dfefd94910e4bac185890f8
Henrique Tavares <ihenrits@gmail.com>
2018-10-11 17:54:57 -0300

    Exibindo as Mensagens de Sucesso e Erro

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
+import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
@@ -10,7 +11,8 @@ const rootReducer = combineReducers({
     dashboard: DashboardReducer, /* dashboard: () => ({summary: {credit: 100, debt: 50}}) */
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
-    form: formReducer
+    form: formReducer,
+    toastr: toastrReducer
 
 })
 
