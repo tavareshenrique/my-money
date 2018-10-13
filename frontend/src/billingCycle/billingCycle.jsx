bdfb52a3588be22ad29962477c8edebb862d8d40
Henrique Tavares <ihenrits@gmail.com>
2018-10-13 15:04:34 -0300

    Alterar Ciclo de Pagamento

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -11,7 +11,7 @@ import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from "../common/tab/tabAction"
-import { create } from './billingCycleActions'
+import { create, update } from './billingCycleActions'
 
 import List from './billingCycleList'
 import Form from './billingCycleForm'
@@ -45,7 +45,7 @@ class BillingCycle extends Component {
                                 <Form onSubmit={ this.props.create } />
                             </TabContent>
                             <TabContent id='tabUpdate' >
-                                <Form />
+                                <Form onSubmit={ this.props.update } />
                             </TabContent>
                             <TabContent id='tabDelete' ><h1>Excluir</h1></TabContent>
                         </TabsContent>
@@ -57,5 +57,5 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create, update }, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
